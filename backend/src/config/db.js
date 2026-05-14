de239d5b79120778beed1304737ad24598dfb860
chelsepit <cf.maranga@gmail.com>
2026-05-14 11:18:59 +0800

    initial project scafold

--- backend/src/config/db.js
+++
@@ -1,5 +0,0 @@
-const { Prisma } = require("@prisma/client/extension")
-
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-module.exports = prisma;
--- backend/src/modules/auth/auth.router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { registerHandler, loginHandler } = require('./auth.controller');
-const router = express.Router();
-router.post('/register', registerHandler);
-router.post('/login', loginHandler);
-module.exports = router;
